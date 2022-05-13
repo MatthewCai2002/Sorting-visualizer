642812c32fb1c1dd4905cd7339e298fdfe2a0720
forbet <matthewcaiuni@gmail.com>
2022-05-12 19:31:47 -0700

    styled array to appear as bars

--- src/SortingVisualizer/SortingVisualizer.js
+++ src/SortingVisualizer/SortingVisualizer.js
@@ -1,14 +1,13 @@
 import "./SortingVisualizer.css"
 
 function SortingVisualizer(props) {
-    const randomArray = (length, max) => [...new Array(length)].map(() => Math.floor(Math.random() * max))
-    let array = randomArray(100, 1000)
+    const randomArray = (length, min, max) => [...new Array(length)].map(() => Math.floor(Math.random()*(max-min+1)+min))
+    let array = randomArray(100, 2, 80)
 
     return (
-        <div>
+        <div className="array-container">
             {array.map((value, idx) => (
-                <div className="val-bar" key={idx}>
-                    {value}
+                <div className="val-bar" key={idx} style={{height: `${value}vh`}}>
                 </div>
             ))}
         </div>
